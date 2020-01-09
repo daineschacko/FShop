75a0a3477114339e3c0e7c5b885efdb727802bb2
ajo <daineschacko06@gmail.com>
2020-01-09 18:28:25 +0530

    ft

--- app/src/main/java/com/example/fshop/Bed_Room.java
+++ app/src/main/java/com/example/fshop/Bed_Room.java
@@ -2,13 +2,31 @@ package com.example.fshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Bed_Room extends AppCompatActivity {
+    Button btn1,btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bed__room);
+        btn1=findViewById(R.id.btn_book);
+        btn2=findViewById(R.id.book_btn2);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Bed_Room.this,payment.class));
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Bed_Room.this,payment.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/fshop/home.java
+++ app/src/main/java/com/example/fshop/home.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.TextView;
 
 public class home extends AppCompatActivity {
-    TextView bed,pay;
+    TextView bed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +21,6 @@ public class home extends AppCompatActivity {
                 startActivity(new Intent( home.this,Bed_Room.class));
             }
         });
-        pay=findViewById(R.id.pay_text);
-        pay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(home.this,payment.class));
-            }
-        });
+
     }
 }
